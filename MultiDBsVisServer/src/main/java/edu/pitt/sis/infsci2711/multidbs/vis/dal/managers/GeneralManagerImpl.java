66018eb1ddf3141fc493b30d9c9bbe463c55f40d
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2015-01-31 20:39:51 -0500

    GeneralManagerImpl
    GeneralManagerImpl

--- MultiDBsVisServer/src/main/java/edu/pitt/sis/infsci2711/multidbs/vis/dal/managers/GeneralManagerImpl.java
+++ MultiDBsVisServer/src/main/java/edu/pitt/sis/infsci2711/multidbs/vis/dal/managers/GeneralManagerImpl.java
@@ -1,5 +1,237 @@
 package edu.pitt.sis.infsci2711.multidbs.vis.dal.managers;
 
-public class GeneralManagerImpl {
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.NonUniqueResultException;
+
+import edu.pitt.sis.infsci2711.multidbs.vis.dal.dao.GenericDAO;
+import edu.pitt.sis.infsci2711.multidbs.vis.dal.utils.HibernateUtil;
+
+/**
+ * @author Evgeny
+ * @param <T>
+ *
+ */
+public abstract class GeneralManagerImpl<Dao extends GenericDAO<T, ID>, T extends Object, ID extends Serializable> implements GeneralManager<T, ID> {
+
+	private static Logger logger = LogManager.getLogger(GeneralManagerImpl.class.getName());
+	
+	protected final Dao _dao;
+	private final Class<?> _clazz;
+	
+	protected GeneralManagerImpl(final Dao dao, final Class<?> clazz) {
+		_dao = dao;
+		_clazz = clazz;
+	}
+	
+	protected void checkIfDaoSet() {
+		if (_dao == null) {
+			logger.error("Dao is not set up");
+			//TODO: create custom exception
+			throw new RuntimeException("Dao is not set up");
+		}
+	}
+	
+	@Override
+	public ID save(final T entity) throws Exception {
+		
+		checkIfDaoSet();
+		
+		try {
+            HibernateUtil.beginTransaction();
+            
+            ID result = _dao.save(entity);
+            
+            HibernateUtil.commitTransaction();
+            
+            return result;
+        } catch (NonUniqueResultException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("save failed NonUniqueResultException", ex);
+            throw ex;
+        } catch (HibernateException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("save failed HibernateException", ex);
+        	throw ex;
+        }
+	}
+
+	@Override
+	public void saveOrUpdate(final T entity) throws Exception {
+		
+		checkIfDaoSet();
+		
+		try {
+            HibernateUtil.beginTransaction();
+            
+            _dao.saveOrUpdate(entity);
+            
+            HibernateUtil.commitTransaction();
+        } catch (NonUniqueResultException ex) {
+            
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("save failed NonUniqueResultException", ex);
+        	throw ex;
+        } catch (HibernateException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("save failed HibernateException", ex);
+        	throw ex;
+        }
+	}
+
+	@Override
+	public T merge(final T entity) throws Exception {
+		
+		checkIfDaoSet();
+		
+		try {
+            HibernateUtil.beginTransaction();
+            
+            T result = _dao.merge(entity);
+            
+            HibernateUtil.commitTransaction();
+            
+            return result;
+        } catch (NonUniqueResultException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("merge failed NonUniqueResultException", ex);
+            throw ex;
+        } catch (HibernateException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("merge failed HibernateException", ex);
+        	throw ex;
+        }
+	}
+
+	@Override
+	public void delete(final T entity) throws Exception {
+		
+		checkIfDaoSet();
+		
+		try {
+            HibernateUtil.beginTransaction();
+            
+            _dao.delete(entity);
+            
+            HibernateUtil.commitTransaction();
+        } catch (NonUniqueResultException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("delete failed NonUniqueResultException", ex);
+        	throw ex;
+        } catch (HibernateException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("delete failed HibernateException", ex);
+        	throw ex;
+        }
+	}
+
+	@Override
+	public List<T> findAll() throws Exception {
+		
+		checkIfDaoSet();
+		
+		List<T> result = null;
+		try {
+            HibernateUtil.beginTransaction();
+            
+            result = _dao.findAll(_clazz);
+            
+            HibernateUtil.commitTransaction();
+        } catch (NonUniqueResultException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("findAll failed NonUniqueResultException", ex);
+        	throw ex;
+        } catch (HibernateException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("findAll failed HibernateException", ex);
+        	throw ex;
+        }
+		return result;
+	}
+
+	@Override
+	public T findByID(final ID id) throws Exception {
+		
+		checkIfDaoSet();
+		
+		T result = null;
+		try {
+            HibernateUtil.beginTransaction();
+            
+            result = _dao.findByID(_clazz, id);
+            
+            HibernateUtil.commitTransaction();
+        } catch (NonUniqueResultException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("findByID failed NonUniqueResultException", ex);
+        	throw ex;
+        } catch (HibernateException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("findByID failed HibernateException", ex);
+        	throw ex;
+        }
+		return result;	
+	}
+	
+	 public static <T extends Object> T initializeField(final T detachedParent, final String... fieldsNames) 
+			 throws Exception {
+		 try {
+			 //TODO:add a check whether field is already initialized
+	 
+			 HibernateUtil.beginTransaction();
+        
+			 @SuppressWarnings("unchecked")
+			 T reattachedParent = (T) HibernateUtil.getSession().merge(detachedParent); 
+        
+	        for (String fieldName : fieldsNames) {
+	        	// get the field from the entity and initialize it
+	            Field fieldToInitialize = detachedParent.getClass().getDeclaredField(fieldName);
+	            fieldToInitialize.setAccessible(true); //TODO: should it be set to false after the init is done?
+	            Object objectToInitialize = fieldToInitialize.get(reattachedParent);
+	
+	            Hibernate.initialize(objectToInitialize);
+	        }
+        
+	        HibernateUtil.commitTransaction();
+        
+	        return reattachedParent;
+        } catch (NonUniqueResultException | NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
+
+        	HibernateUtil.rollbackTransaction();
+        	
+        	logger.error("findByID failed NonUniqueResultException", ex);
+        	throw ex;
+        }
+	 }
 }
+
+
