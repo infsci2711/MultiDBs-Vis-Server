4df6c8247dd0e5a7c7d4e5185ac8dc7127457206
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2015-02-08 17:40:58 -0500

    Canvas View Model
    Canvas View Model

---
+++ MultiDBsVisServer/src/main/java/edu/pitt/sis/infsci2711/multidbs/vis/dal/viewmodels/CanvasViewModel.java
@@ -0,0 +1,66 @@
+package edu.pitt.sis.infsci2711.multidbs.vis.dal.viewmodels;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import edu.pitt.sis.infsci2711.multidbs.vis.dal.orm.Users;
+
+@XmlRootElement
+public class CanvasViewModel {
+   
+	private int vid;
+	private String name;
+	private Date cdate;
+	private Date mdate;
+	private int privilege;
+	private Users users;
+	
+	public int getVid(){
+		return vid;
+	}
+	
+	public void setVid(final int vid){
+		this.vid = vid;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(final String name){
+		this.name = name;
+	}
+	
+	public Date getCdate(){
+		return cdate;
+	}
+	
+	public void setCdate(final Date cdate){
+		this.cdate = cdate;
+	}
+	
+	public Date getMdate(){
+		return mdate;
+	}
+	
+	public void setMdate(final Date mdate){
+		this.mdate = mdate;
+	}
+	
+	public int getPrivilege(){
+		return privilege;
+	}
+	
+	public void setPrivilege(final int privilege){
+		this.privilege = privilege;
+	}
+	
+	public Users getUsers(){
+		return users;
+	}
+	
+	public void setUsers(final Users users){
+		this.users = users;
+	}
+}
