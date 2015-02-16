4dec321a2826da9c8016855ffaf1070458671f5c
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2015-02-15 22:02:48 -0500

    VisualizationBL after adding charts
    Complete functions of charts in VisualizationBL

--- MultiDBsVisServer/src/main/java/edu/pitt/sis/infsci2711/multidbs/vis/bll/VisualizationBL.java
+++ MultiDBsVisServer/src/main/java/edu/pitt/sis/infsci2711/multidbs/vis/bll/VisualizationBL.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
-import jnr.ffi.Struct.int16_t;
 import edu.pitt.sis.infsci2711.multidbs.vis.dal.managers.CanvasesManager;
 import edu.pitt.sis.infsci2711.multidbs.vis.dal.managers.CanvasesManagerImpl;
 import edu.pitt.sis.infsci2711.multidbs.vis.dal.managers.ChartsManager;
@@ -13,9 +12,10 @@ import edu.pitt.sis.infsci2711.multidbs.vis.dal.managers.ChartsManagerImpl;
 import edu.pitt.sis.infsci2711.multidbs.vis.dal.managers.UserManager;
 import edu.pitt.sis.infsci2711.multidbs.vis.dal.managers.UserManagerImpl;
 import edu.pitt.sis.infsci2711.multidbs.vis.dal.orm.Canvases;
-import edu.pitt.sis.infsci2711.multidbs.vis.dal.orm.ColfusionCharts;
+import edu.pitt.sis.infsci2711.multidbs.vis.dal.orm.Charts;
 import edu.pitt.sis.infsci2711.multidbs.vis.dal.orm.Users;
 import edu.pitt.sis.infsci2711.multidbs.vis.dal.viewmodels.CanvasViewModel;
+import edu.pitt.sis.infsci2711.multidbs.vis.dal.viewmodels.ChartViewModel;
 
 public class VisualizationBL {
 	
@@ -28,6 +28,17 @@ public class VisualizationBL {
 		
 		return canvasVM;
 		
+	}
+	
+	public ChartViewModel createChartViewModel(Integer cid, String name, String type, Canvases canvases){
+		ChartViewModel chartVM = new ChartViewModel();
+		
+		chartVM.setCid(cid);
+		chartVM.setName(name);
+		chartVM.setType(type);
+		chartVM.setCanvases(canvases);
+		
+		return chartVM;
 	}
 
 	public CanvasViewModel createCanvase(int userId, String canvasName) throws Exception{
@@ -74,14 +85,15 @@ public class VisualizationBL {
 		
 	}
 	
-	public ColfusionCharts createChart(int canvasId, String chartName, String type) throws Exception{
+	public ChartViewModel createChart(int canvasId, String chartName, String type) throws Exception{
 		CanvasesManager canvasMng = new CanvasesManagerImpl();
 		Canvases canvases = canvasMng.findByID(canvasId);
 		
 		ChartsManager chartManager = new ChartsManagerImpl();
-		ColfusionCharts newChart = chartManager.createNewChart(canvases, chartName, type);
+		Charts newChart = chartManager.createNewChart(canvases, chartName, type);
+		ChartViewModel chartVM = createChartViewModel(newChart.getCid(), newChart.getName(), newChart.getType(), newChart.getCanvases());
 		
-		return newChart;
+		return chartVM;
 		
 	}
 	
@@ -92,12 +104,32 @@ public class VisualizationBL {
 		
 	}
 	
-	public List<ColfusionCharts> findByChartName(String name) throws Exception{
+	public List<ChartViewModel> findByChartName(String name) throws Exception{
 		
 		ChartsManager chartMng = new ChartsManagerImpl();
-		List<ColfusionCharts> chartsList = chartMng.findByName(name);
+		List<Charts> chartsList = chartMng.findByName(name);
+		Iterator<Charts> itCharts = chartsList.iterator();
+		List<ChartViewModel> chartsVMList = new ArrayList<ChartViewModel>();
 		
-		return chartsList;
+		while(itCharts.hasNext()){
+			Charts charts = itCharts.next();
+			
+			ChartViewModel chartsVM = createChartViewModel(charts.getCid(), charts.getName(), charts.getType(), charts.getCanvases());
+			
+			chartsVMList.add(chartsVM);
+		}
+	
+		return chartsVMList;
+	}
+	
+	public ChartViewModel findByChartId(int id) throws Exception{
+		
+		ChartsManager chartMng = new ChartsManagerImpl();
+		Charts chart = chartMng.findByID(id);
+		
+		ChartViewModel chartsVM = createChartViewModel(chart.getCid(), chart.getName(), chart.getType(), chart.getCanvases());
+		
+		return chartsVM;
 	}
 }
 
