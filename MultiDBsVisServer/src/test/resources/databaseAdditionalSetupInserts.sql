488d5d745a20b2841567b434e4a4cf4a10f58ea0
rogerxi <rogerxip@gmail.com>
2015-03-20 19:45:52 -0400

    Revert "Pom"
    This reverts commit f447814e5f3474fb8955d14174a4c3f4d13885d2.

--- MultiDBsVisServer/src/test/resources/databaseAdditionalSetupInserts.sql
+++ MultiDBsVisServer/src/test/resources/databaseAdditionalSetupInserts.sql
@@ -1,1 +1,1 @@
-INSERT INTO `users` (`user_id`, `user_names`, `user_email`,`user_modification`, `user_date`, `user_lastlogin`) VALUES ('1', 'test', 'pex7@pitt.edu', '2015-02-14 18:32:57	', '2014-06-19 14:05:24	', '2015-02-14 18:32:57	');
+INSERT INTO `users` (`user_id`, `user_names`, `user_email`,`user_modification`, `user_date`, `user_lastlogin`) VALUES ('1', 'test', 'epk8@pitt.edu', '2015-02-01 18:32:57	', '2014-06-19 14:05:24	', '2015-02-01 18:32:57	');
