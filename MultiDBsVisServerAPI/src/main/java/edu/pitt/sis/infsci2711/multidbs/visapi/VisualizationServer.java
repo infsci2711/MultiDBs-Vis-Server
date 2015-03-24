e46c5f1341c2cbbb5e28d304227fe485e74c4aab
rogerxi <rogerxip@gmail.com>
2015-03-23 20:43:52 -0400

    server change to 7654

--- MultiDBsVisServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbs/visapi/VisualizationServer.java
+++ MultiDBsVisServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbs/visapi/VisualizationServer.java
@@ -4,7 +4,7 @@ import edu.pitt.sis.infsci2711.multidbs.utils.JerseyJettyServer;
 
 public class VisualizationServer {
 	public static void main(final String[] args) throws Exception {
-		JerseyJettyServer server = new JerseyJettyServer(7777, "edu.pitt.sis.infsci2711.multidbs.visapi.rest");
+		JerseyJettyServer server = new JerseyJettyServer(7654, "edu.pitt.sis.infsci2711.multidbs.visapi.rest");
 		server.start();
 	}
 }
