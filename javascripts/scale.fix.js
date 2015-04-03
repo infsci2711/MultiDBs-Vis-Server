3e0260178ff82154824f14ed1818a7c5287c27bb
Chi-Lu Tsao <QIC27@pitt.edu>
2015-04-03 19:51:29 -0400

    Editing Github Homepage
    Added JavaScript function, CSS, and the navigation part

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -18,3 +18,10 @@ fixScale = function(doc) {
 	}
 
 };
+
+function show(shown, hidden1, hidden2) {
+  document.getElementById(shown).style.display='block';
+  document.getElementById(hidden1).style.display='none';
+  document.getElementById(hidden2).style.display='none';
+  return false;
+}
