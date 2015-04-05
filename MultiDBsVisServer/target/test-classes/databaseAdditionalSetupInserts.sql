6681a06a1afa40f41c9f25c7cabad41f1d950046
KieferTowne <kefei.tong@gmail.com>
2015-04-05 00:49:02 -0400

    Add material for the describe page
    Add describe material into the describe page

---
+++ MultiDBsVisServer/target/test-classes/databaseAdditionalSetupInserts.sql
@@ -0,0 +1,1 @@
+INSERT INTO `users` (`user_id`, `user_names`, `user_email`,`user_modification`, `user_date`, `user_lastlogin`) VALUES ('1', 'test', 'epk8@pitt.edu', '2015-02-01 18:32:57	', '2014-06-19 14:05:24	', '2015-02-01 18:32:57	');
