8df2a761d3bd75bb583e4ca63317a6196f50707b
Chi-Lu Tsao <QIC27@pitt.edu>
2015-04-18 22:22:16 -0400

    Finalize all the codes

--- MultiDBsVisServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbs/visapi/VisualizationServer.java
+++ MultiDBsVisServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbs/visapi/VisualizationServer.java
@@ -1,10 +1,27 @@
 package edu.pitt.sis.infsci2711.multidbs.visapi;
 
+import java.io.File;
+
 import edu.pitt.sis.infsci2711.multidbs.utils.JerseyJettyServer;
+import edu.pitt.sis.infsci2711.multidbs.utils.PropertiesManager;
 
 public class VisualizationServer {
+	private final static String PROPERTY_PORT = "port";
+	private final static int DEFAULT_PORT = 7654;
+	
 	public static void main(final String[] args) throws Exception {
-		JerseyJettyServer server = new JerseyJettyServer(7777, "edu.pitt.sis.infsci2711.multidbs.visapi.rest");
+		//JerseyJettyServer server = new JerseyJettyServer(7654, "edu.pitt.sis.infsci2711.multidbs.visapi.rest");
+		//server.start();
+		
+		if (args.length > 0) {
+			String propertiesFilePath = args[0];
+			File propertiesFile = new File(propertiesFilePath);
+			PropertiesManager.getInstance().loadProperties(propertiesFile);
+		}
+		
+		final JerseyJettyServer server = new JerseyJettyServer(PropertiesManager.getInstance().getIntProperty(PROPERTY_PORT, DEFAULT_PORT), 
+				"edu.pitt.sis.infsci2711.multidbs.visapi.rest");
+		
 		server.start();
 	}
 }
