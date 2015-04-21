5f00d45c2e6c129f7ebdd855bc94a5d3102c666c
Chi-Lu Tsao <QIC27@pitt.edu>
2015-04-21 13:10:58 -0400

    fixed a typo

--- startup.sh
+++ startup.sh
@@ -62,7 +62,7 @@ mvn install
 #Start the server (Go to Multidbs-Vis-Server/MultidbsVisServerAPI/target)
 cd /opt/project/MultiDBs-Vis-Server/MultidbsVisServerAPI/target
 
-nohup java -jar multidbsvisserverapi-0.1-SNAPSHOT.jar /opt/project/Multidbs-Vis-Server/MultidbsVisServerAPI/src/main/resources/config.properties > log.out 2> error.log < /dev/null &
+nohup java -jar multidbsvisserverapi-0.1-SNAPSHOT.jar /opt/project/MultiDBs-Vis-Server/MultiDBsVisServerAPI/src/main/resources/config.properties > log.out 2> error.log < /dev/null &
 
 
 
