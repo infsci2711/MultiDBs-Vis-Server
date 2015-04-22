9d66af21b36a993484fc7df17fc7a37229d068b1
Chi-Lu Tsao <QIC27@pitt.edu>
2015-04-22 01:25:28 -0400

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,20 +1,7 @@
-#Maven install
-cd ..
-cd ColFusionDockerUtils
-mvn install
-cd ..
-cd ColFusionUtils
-mvn install
-cd ..
-cd MultiDBs-Utils
-mvn install
-cd ..
-cd MultiDBs-Vis-Server
-mvn install
+﻿#!/bin/bash
 
 
 #Start the server (Go to Multidbs-Vis-Server/MultidbsVisServerAPI/target)
-cd MultidbsVisServerAPI/target
 nohup java -jar multidbsvisserverapi-0.1-SNAPSHOT.jar /opt/project/MultiDBs-Vis-Server/MultiDBsVisServerAPI/src/main/resources/config.properties > log.out 2> error.log < /dev/null &
 
 
